5f52bf217bd38b6a0e69dc7464f8868696f1471e
BocilAlok <88170940+BocilAlok@users.noreply.github.com>
2022-02-18 14:02:32 +0700

    Add files via upload

--- smbf.py
+++ smbf.py
@@ -55,10 +55,10 @@ _U_ = "\x1b[0;91m" # Ungu
 
 ### Logo
 _logo_line_1_ = ('%s ___  __  __ ___ ___ '%(_P_))
-_logo_line_2_ = ('%s|   \|  \/  | _ ) __| %s┌─────────────────────────┐'%(_P_,_U_))
+_logo_line_2_ = ('%s|   \|  \/  | _ ) __| %s┌──────────────────────────┐'%(_P_,_U_))
 _logo_line_3_ = ('%s| |) | |\/| | _ \ _|  %s│   %s• Code By Sanz •        %s│'%(_P_,_U_,_P_,_U_))
 _logo_line_4_ = ('%s|___/|_|  |_|___/_|   %s│ %sGithub.com/BocilAlok/smbf %s│'%(_P_,_U_,_P_,_U_))
-_logo_line_5_ = ('%s XNSCODE Team 2021    └─────────────────────────┘'%(_U_))
+_logo_line_5_ = ('%s XNSCODE Team 2021    └────────────────────────────┘'%(_U_))
 def _my_logo_():
     _dapunta_cici_(_logo_line_1_)
     _dapunta_cici_(_logo_line_2_)
